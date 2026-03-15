516bf041000e413ac388fe97a6c70e447a76d929
mmorales170561 <m.morales.170561@gmail.com>
2026-03-14 18:55:16 -0700

    Update powers.sh

--- powers.sh
+++ powers.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
-
-# ENSURE SYSTEM PATH INCLUDES OUR TOOLS
 export PATH=$PATH:/tmp/bin
 
 observer() {
-    echo ">> [SYS_LOG] INITIALIZING SUBDOMAIN_SCAN..."
     subfinder -d "$1" -silent | grep -vE "${OUT_SCOPE//, /|}"
 }
 
 kingpin() {
-    echo ">> [SYS_LOG] SCANNING_FOR_KINGPIN_ASSETS..."
     subfinder -d "$1" -silent | httpx -silent
 }
 
 automated_hunt() {
-    echo ">> [SYS_LOG] ENGAGING NUCLEI_HUNT..."
     subfinder -d "$1" -silent | nuclei -silent
 }
